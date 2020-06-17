a96ad512f7a8d86d5c890263b0d066a90436260b
king <2193794926@qq.com>
2020-06-17 16:23:38 +0800

    第一次

--- urls.py
+++ urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     url(r'^', include('home_application.urls')),
     # 如果你习惯使用 mako 模板，请在 mako_application 里开发你的应用，
     # 这里的 mako_application 可以改成你想要的名字
-    url(r'^mako/', include('mako_application.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n'))
 ]
